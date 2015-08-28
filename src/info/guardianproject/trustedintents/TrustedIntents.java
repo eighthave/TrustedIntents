aa4af2037bcb3e4541166f4ada8f842f53ee2d64
Hans-Christoph Steiner <hans@eds.org>
2015-08-28 14:30:07 +0200

    a trusted chooser that mimics Intent.createChooser()
    This creates a chooser that places the trusted recipients at the top of the
    list of options.  But careful! It does not remove the untrusted recipients
    from the list because the API does not allow for that.

--- src/info/guardianproject/trustedintents/TrustedIntents.java
+++ src/info/guardianproject/trustedintents/TrustedIntents.java
@@ -1,32 +1,41 @@
 
 package info.guardianproject.trustedintents;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.ClipData;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.LabeledIntent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.content.pm.Signature;
+import android.os.Build;
+import android.os.Parcelable;
 import android.text.TextUtils;
 
 import java.lang.reflect.Constructor;
 import java.security.cert.CertificateException;
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 
 public class TrustedIntents {
 
     private static TrustedIntents instance;
 
+    private static Context context;
     private static PackageManager pm;
 
     private final LinkedHashSet<ApkSignaturePin> pinList;
 
-    private TrustedIntents(Context context) {
+    private TrustedIntents(Context c) {
+        context = c.getApplicationContext();
         pm = context.getPackageManager();
         this.pinList = new LinkedHashSet<ApkSignaturePin>();
     }
@@ -45,6 +54,85 @@ public class TrustedIntents {
      */
     public boolean isReceiverTrusted(ResolveInfo resolveInfo) {
         return isPackageNameTrusted(resolveInfo.activityInfo.packageName);
+    }
+
+    /**
+     * Creates an {@link Intent#ACTION_CHOOSER ACTION_CHOOSER} {@link Intent}
+     * that puts trusted apps at the top of the list. Careful, it does not
+     * filter out untrusted apps!
+     *
+     * @param target The {@link Intent} that the user will be selecting an
+     *            activity to perform.
+     * @param title Optional title that will be displayed in the chooser.
+     * @return Return a new Intent object that you can hand to
+     *         {@link Context#startActivity(Intent) Context.startActivity()} and
+     *         related methods.
+     */
+    @SuppressLint("NewApi")
+    public Intent createChooser(Intent target, CharSequence title) {
+        Intent intent = new Intent(Intent.ACTION_CHOOSER);
+        intent.putExtra(Intent.EXTRA_INTENT, target);
+        if (title != null) {
+            intent.putExtra(Intent.EXTRA_TITLE, title);
+        }
+
+        List<LabeledIntent> targetedIntents = new ArrayList<LabeledIntent>();
+        List<ResolveInfo> resolvedActivities = pm.queryIntentActivities(target, 0);
+        if (!resolvedActivities.isEmpty()) {
+            for (ResolveInfo resolveInfo : resolvedActivities) {
+                if (isReceiverTrusted(resolveInfo)) {
+                    String packageName = resolveInfo.activityInfo.packageName;
+                    LabeledIntent includedIntent = new LabeledIntent(target,
+                            context.getPackageName(),
+                            "(trusted)", android.R.drawable.star_big_on);
+                    includedIntent.setPackage(packageName);
+                    targetedIntents.add(includedIntent);
+                }
+            }
+        }
+
+        intent.putExtra(Intent.EXTRA_INITIAL_INTENTS,
+                targetedIntents.toArray(new Parcelable[] {}));
+
+        if (Build.VERSION.SDK_INT < 16)
+            return intent; // the ClipData stuff was added in 4.1/android-16
+
+        // Migrate any clip data and flags from target.
+        int permFlags = target.getFlags()
+                & (Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        if (permFlags != 0) {
+            ClipData targetClipData = target.getClipData();
+            if (targetClipData == null && target.getData() != null) {
+                ClipData.Item item = new ClipData.Item(target.getData());
+                String[] mimeTypes;
+                if (target.getType() != null) {
+                    mimeTypes = new String[] {
+                            target.getType()
+                    };
+                } else {
+                    mimeTypes = new String[] {};
+                }
+                targetClipData = new ClipData(null, mimeTypes, item);
+            }
+            if (targetClipData != null) {
+                intent.setClipData(targetClipData);
+                intent.addFlags(permFlags);
+            }
+        }
+
+        return intent;
+    }
+
+    public boolean isReceiverTrusted(String packageName) {
+        try {
+            checkTrustedSigner(packageName);
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (CertificateException e) {
+            return false;
+        }
+        return true;
     }
 
     /**
